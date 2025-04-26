ddd27c207f169e9bad26b31b3126f1f4d9a600ad
MAIPA01 <marcelipatryk@gmail.com>
2025-04-26 17:57:42 +0200

    Enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,7 +5,13 @@ public class Enemy : MonoBehaviour
 {
     public EnemyPatrol patrol;
     public Rigidbody2D rb;
+    public float followSpeed;
     public bool isRight = true;
+
+    [TagField]
+    public string playerTag;
+
+    public GameObject followTarget;
 
     void Start()
     {
@@ -14,6 +20,13 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (followTarget != null)
+        {
+            Vector2 dir = new Vector2(followTarget.transform.position.x - transform.position.x, 0).normalized;
+
+            rb.linearVelocity = dir * followSpeed;
+        }
+
         if (rb.linearVelocityX > 0 && !isRight)
         {
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
@@ -23,6 +36,24 @@ public class Enemy : MonoBehaviour
         {
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             isRight = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag) && followTarget == null)
+        {
+            followTarget = collision.gameObject;
+            patrol.StopPatrol();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag) && followTarget != null)
+        {
+            followTarget = null;
+            patrol.StartPatrol();
         }
     }
 }
